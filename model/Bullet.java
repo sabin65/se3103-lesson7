0787b1cd7f06499bc67c69ebd232a58bde620182
Sabin Biju <sabinbiju9@gmail.com>
2022-04-01 11:27:04 -0500

    final

---
+++ model/Bullet.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Bullet extends GameElement {
+    public static final int WIDTH = 5, UNIT_MOVE = 10;
+
+    public Bullet(int x, int y) {
+        super(x, y, Color.red, true, WIDTH, WIDTH * 3);
+    }
+
+    @Override
+    public void render(Graphics2D g2) {
+        g2.setColor(color);
+        if (filled) g2.fillRect(x, y, width, height);
+        else g2.drawRect(x, y, width, height);
+    }
+
+    @Override
+    public void animate() {
+        super.y -= UNIT_MOVE;
+    }
+    
+}
